814d3321d41545307672bdca2ab3114fb5cebc48
Slava137 <slava13071995@gmail.com>
2022-12-22 23:56:25 +0300

    feat: add buttons for filters

--- src/components/view/pages/main/filters/filters.ts
+++ src/components/view/pages/main/filters/filters.ts
@@ -1,8 +1,29 @@
-// import type Data from '../../../../types/types';
+import Component from '../../../../templates/components';
 
-class Filters {
+class Filters extends Component {
+    constructor(tagName: string, className: string) {
+        super(tagName, className);
+    }
+
     draw(): void {
-        
+        const buttonsBlock: HTMLDivElement = document.createElement('div');
+        buttonsBlock.className = 'filters-buttons';
+        this.container.append(buttonsBlock);
+
+        const resetButton: HTMLButtonElement = document.createElement('button');
+        resetButton.className = 'button button_filters';
+        resetButton.textContent = 'Reset filters';
+        buttonsBlock.append(resetButton);
+
+        const copyButton: HTMLButtonElement = document.createElement('button');
+        copyButton.className = 'button button_filters';
+        copyButton.textContent = 'Copy settings';
+        buttonsBlock.append(copyButton);
+    }
+
+    render(): HTMLElement {
+        this.draw();
+        return this.container;
     }
 }
 export default Filters;
