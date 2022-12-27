acbdc9c5eca29c372310a6892cd53cd7f440a1cc
Vlad Tarnovski <vladtarnovski989@gmail.com>
2022-12-27 18:26:07 +0300

    refactor: change data source in view filter

--- src/components/view/pages/main/filters/getListsOfPropData.ts
+++ src/components/view/pages/main/filters/getListsOfPropData.ts
@@ -1,12 +1,14 @@
-import data from '../../../../../data.json';
+// import data from '../../../../../data.json';
+import { productData } from '../../../../model/data';
+
 import { DataObject, DataValueEachOfCategory } from '../../../../types/types';
 
 function getListOfCategories(): Set<string> {
-  const arrayProducts: DataObject[] = data.products;
+  const arrayProducts: DataObject[] = productData.products;
   const objectCategory: Set<string> = new Set();
-  
+
   for (let i = 0; i < arrayProducts.length; i++) {
-    objectCategory.add(arrayProducts[i].category)
+    objectCategory.add(arrayProducts[i].category);
   }
 
   return objectCategory;
@@ -16,7 +18,7 @@ export const listCategories: Set<string> = getListOfCategories();
 
 function getListOfValueEachCategory(): DataValueEachOfCategory {
   const listValueOfEachCategory: DataValueEachOfCategory = {};
-  const arrayProducts: DataObject[] = data.products;
+  const arrayProducts: DataObject[] = productData.products;
 
   listCategories.forEach((key: string): void => {
     listValueOfEachCategory[key] = 0;
@@ -34,11 +36,11 @@ function getListOfValueEachCategory(): DataValueEachOfCategory {
 export const valueEachCategory: DataValueEachOfCategory = getListOfValueEachCategory();
 
 function getListOfBrands(): Set<string> {
-  const arrayProducts: DataObject[] = data.products;
+  const arrayProducts: DataObject[] = productData.products;
   const objectBrand: Set<string> = new Set();
-  
+
   for (let i = 0; i < arrayProducts.length; i++) {
-    objectBrand.add(arrayProducts[i].brand)
+    objectBrand.add(arrayProducts[i].brand);
   }
 
   return objectBrand;
@@ -48,7 +50,7 @@ export const listOfBrands: Set<string> = getListOfBrands();
 
 function getListOfValueEachBrand(): DataValueEachOfCategory {
   const listValueOfEachBrand: DataValueEachOfCategory = {};
-  const arrayProducts: DataObject[] = data.products;
+  const arrayProducts: DataObject[] = productData.products;
 
   listOfBrands.forEach((key: string): void => {
     listValueOfEachBrand[key] = 0;
